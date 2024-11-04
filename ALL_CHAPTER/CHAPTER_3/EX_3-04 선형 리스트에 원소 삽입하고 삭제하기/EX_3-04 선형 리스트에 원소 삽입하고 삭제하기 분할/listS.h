c99a898190c436ec1da9c6be904f9ea1ac6d182f
ijigu <75564572+zidudu@users.noreply.github.com>
2024-11-04 11:32:03 +0900

    Update listS.h

--- ALL_CHAPTER/CHAPTER_3/EX_3-04 선형 리스트에 원소 삽입하고 삭제하기/EX_3-04 선형 리스트에 원소 삽입하고 삭제하기 분할/listS.h
+++ ALL_CHAPTER/CHAPTER_3/EX_3-04 선형 리스트에 원소 삽입하고 삭제하기/EX_3-04 선형 리스트에 원소 삽입하고 삭제하기 분할/listS.h
@@ -1,1 +1,7 @@
+#pragma once //헤더의 중복선언 방지
+#define MAX 10 //상수 선언 //MAX는 전역변수임
+
+int insertElement(int L[], int n, int x); //선형 리스트 원소 삽입
+int deleteElement(int L[], int n, int x); //선형 리스트 원소 삭제
+
 
